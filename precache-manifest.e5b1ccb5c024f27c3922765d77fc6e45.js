65d9f3b8b6a7bfa6face251b70f610852cdc5bc2
Evgenii <evgeniy.ru_94@mail.ru>
2021-01-18 09:55:22 +0300

    Updates

---
+++ precache-manifest.e5b1ccb5c024f27c3922765d77fc6e45.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f07ee4e699bd6ee308eae25ae48e17",
+    "url": "/bordio/index.html"
+  },
+  {
+    "revision": "76fa85093cbb4221c68f",
+    "url": "/bordio/static/css/main.51c8b2a6.chunk.css"
+  },
+  {
+    "revision": "d0a82ae0eee3d274e6ce",
+    "url": "/bordio/static/js/2.e5b64a84.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/bordio/static/js/2.e5b64a84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76fa85093cbb4221c68f",
+    "url": "/bordio/static/js/main.f06aef10.chunk.js"
+  },
+  {
+    "revision": "7d01f38c6d8d1d5164c8",
+    "url": "/bordio/static/js/runtime-main.603b8d57.js"
+  },
+  {
+    "revision": "f3c3a424565c9ac435498f36f989ce8d",
+    "url": "/bordio/static/media/Group 845.f3c3a424.svg"
+  },
+  {
+    "revision": "f50dd28a7493356d91aafce09981f90b",
+    "url": "/bordio/static/media/Group 846.f50dd28a.svg"
+  },
+  {
+    "revision": "e711236eac1bf0ac3d5eadb596b8324d",
+    "url": "/bordio/static/media/Layer 2.e711236e.svg"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/bordio/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
